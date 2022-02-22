f9c0ea6d68a6d24f2bd351bd0f6ee00619168e83
Nathan King <nathanking3590@gmail.com>
2022-02-22 10:00:53 +0000

    add comment to index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './client/App';
 
+// Let's gooooooooo
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App 
   </React.StrictMode>,
   document.getElementById('root')
 );
